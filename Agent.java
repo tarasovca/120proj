faa6e9ade924a9fe12e394bf746006b6d60536a3
tarasovca <95153412+tarasovca@users.noreply.github.com>
2024-03-12 01:45:01 -0700

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -19,8 +19,8 @@ public class Agent implements Steppable {
 
 	// new variables
 	double compliance; // probability of quarantine compliance
-	boolean inQuarantine; 
-	int sickTime = 0; // # of steps since becoming infected 
+	boolean inQuarantine;
+	int sickTime = 0; // # of steps since becoming infected
 	// SEIR statuses
 	Environment.Status status;
 
@@ -31,32 +31,31 @@ public class Agent implements Steppable {
 		this.y = y;
 		this.xdir = xdir;
 		this.ydir = ydir;
-		this.status = status; 
+		this.status = status;
 		this.compliance = compliance;
 		this.inQuarantine = false;
 	}
 
-	
 	@Override
 	public void step(SimState state) {
-		Environment env = (Environment)state;
-		
-		if (this.status == Environment.Status.EXPOSED){
+		Environment env = (Environment) state;
+
+		if (this.status == Environment.Status.EXPOSED) {
 			this.status = Environment.Status.INFECTED;
 			return; // might need to refine logic?
 		}
-		// Check quarantine 
-		if(this.status == Environment.Status.INFECTED){
-				this.sickTime++;
-				if (env.clock >= env.burninTime && this.inQuarantine){
-					checkRecover(env);
-						if (this.sickTime > env.quarantineTime){
-							this.inQuarantine = false;
-						}
+		// Check quarantine
+		if (this.status == Environment.Status.INFECTED) {
+			this.sickTime++;
+			if (env.clock >= env.burninTime && this.inQuarantine) {
+				checkRecover(env);
+				if (this.sickTime > env.quarantineTime) {
+					this.inQuarantine = false;
 				}
-
 			}
-		if (!this.inQuarantine){
+
+		}
+		if (!this.inQuarantine) {
 			move(env);
 			if (this.status == Status.SUSCEPTIBLE) {
 				Bag neighbors = findSickNeighbors(env);
@@ -65,49 +64,46 @@ public class Agent implements Steppable {
 		}
 	}
 
-	
 	void checkQuarantine(Environment env) {
-        if (env.random.nextBoolean(compliance)) {
-            this.inQuarantine = true;
-        }
-    }
-	void checkRecover (Environment env){
-			int recoveryTime = env.recoveryTime + env.random.nextInt(2 * env.recoveryError) - env.recoveryError;
-        	if (this.sickTime >= recoveryTime && env.random.nextBoolean(env.recoveryRate)) {
-            this.status = Environment.Status.RECOVERED;
-            this.inQuarantine = false;
-        }
-    }
-	//double check move
+		if (env.random.nextBoolean(compliance)) {
+			this.inQuarantine = true;
+		}
+	}
+
+	void checkRecover(Environment env) {
+		int recoveryTime = env.recoveryTime + env.random.nextInt(2 * env.recoveryError) - env.recoveryError;
+		if (this.sickTime >= recoveryTime && env.random.nextBoolean(env.recoveryRate)) {
+			this.status = Environment.Status.RECOVERED;
+			this.inQuarantine = false;
+		}
+	}
+
+	// double check move
 	public void move(Environment state) {
-		if(!state.random.nextBoolean(state.getpActive())) {
+		if (!state.random.nextBoolean(state.getpActive())) {
 			return;
 		}
-		if(state.random.nextBoolean(state.getP())) {
-			xdir = state.random.nextInt(3)-1;
-			ydir = state.random.nextInt(3)-1;
+		if (state.random.nextBoolean(state.getP())) {
+			xdir = state.random.nextInt(3) - 1;
+			ydir = state.random.nextInt(3) - 1;
 		}
 		placeAgent(state);
-		
+
 	}
 
 	// implement TODO
-	Bag findSickNeighbors (Environment env){
-
-	}
-	void interact (Environment env, Bag neighbors){
+	Bag findSickNeighbors(Environment env) {
 
 	}
 
+	void interact(Environment env, Bag neighbors) {
 
+	}
 
-	
 	public void placeAgent(Environment state) {
 		x = state.sparseSpace.stx(x + xdir);
 		y = state.sparseSpace.stx(y + ydir);
 		state.sparseSpace.setObjectLocation(this, x, y);
 	}
 
-	
-	
 }
